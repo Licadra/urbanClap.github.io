126ad5f2ec9b0d0da20b762a930d5a5db1904474
licadraEditor <licadraeditor@yandex.com>
2019-10-26 19:10:36 +0300

    updated HM

--- index2.js
+++ index2.js
@@ -22,6 +22,31 @@ $( window ).resize( function () {
 
 
 
+
+
+$(document).ready(function() {
+   // Transition effect for navbar
+   $(window).scroll(function() {
+
+      
+
+      // checks if window is scroll is less than 80px, adds/removes solid class
+      if($(this).scrollTop() > 1){
+
+         $(".navbar").addClass("start");
+        
+      } else {
+         $(".navbar").removeClass("solid");
+      }
+   });
+});
+
+
+
+
+
+
+
 $(document).ready(function() {
    // Transition effect for navbar
    $(window).scroll(function() {
@@ -29,7 +54,8 @@ $(document).ready(function() {
        if($(this).scrollTop() > 100){
             $(".navbar").removeClass("solid");
        }
-      // checks if window is scrolled more than 500px, adds/removes solid class
+
+      // checks if window is scrolled more than 575px, adds/removes solid class
       if ($(this).scrollTop() > 575) {
          $(".navbar").addClass("solid");
       } else {
